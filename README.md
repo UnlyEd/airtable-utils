751ff10697b04c9f0b32bf181eba1a01563d48d2
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-05-18 18:37:27 +0200

    Add Slug formula

---
+++ README.md
@@ -0,0 +1,44 @@
+<a href="https://unly.org"><img src="https://storage.googleapis.com/unly/images/ICON_UNLY.png" align="right" height="20" alt="Unly logo" title="Unly logo" /></a>
+
+Airtable utilities
+===
+
+This repo is mostly about Airtable utilities to centralise formulas and such.
+
+---
+
+# License
+
+[MIT](LICENSE)
+
+---
+
+# Vulnerability disclosure
+
+[See our policy](https://github.com/UnlyEd/Unly).
+
+---
+
+# Contributors and maintainers
+
+This project is being maintained by:
+- [Unly] Ambroise Dhenain ([Vadorequest](https://github.com/vadorequest)) **(active)**
+
+---
+
+# **[ABOUT UNLY]** <a href="https://unly.org"><img src="https://storage.googleapis.com/unly/images/ICON_UNLY.png" height="40" align="right" alt="Unly logo" title="Unly logo" /></a>
+
+> [Unly](https://unly.org) is a socially responsible company, fighting inequality and facilitating access to higher education.
+> Unly is committed to making education more inclusive, through responsible funding for students.
+
+We provide technological solutions to help students find the necessary funding for their studies.
+
+We proudly participate in many TechForGood initiatives. To support and learn more about our actions to make education accessible, visit :
+- https://twitter.com/UnlyEd
+- https://www.facebook.com/UnlyEd/
+- https://www.linkedin.com/company/unly
+- [Interested to work with us?](https://jobs.zenploy.io/unly/about)
+
+Tech tips and tricks from our CTO on our [Medium page](https://medium.com/unly-org/tech/home)!
+
+#TECHFORGOOD #EDUCATIONFORALL
---
+++ formulas/slug.js
@@ -0,0 +1,131 @@
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+SUBSTITUTE(
+  TRIM(LOWER(labelFR)),
+"á", "a"),
+"à", "a"),
+"â", "a"),
+"å", "a"),
+"æ", "ae"),
+"ç", "c"),
+"è", "e"),
+"é", "e"),
+"ë", "e"),
+"ê", "e"),
+"î", "i"),
+"ï", "i"),
+"ì", "i"),
+"ñ", "n"),
+"ô", "o"),
+"ö", "o"),
+"ò", "o"),
+"ø", "o"),
+"õ", "o"),
+"ù", "u"),
+"û", "u"),
+"ü", "u"),
+"ý", "y"),
+" ", "-"),
+"!", ""),
+".", "-"),
+"/", "-"),
+"\"", "-"),
+"#", "-"),
+"$", "-"),
+"%", "-"),
+"&", "-"),
+"'", "-"),
+"(", ""),
+")", ""),
+"*", "-"),
+"+", "-"),
+",", "-"),
+":", "-"),
+";", "-"),
+"<", ""),
+"=", ""),
+">", ""),
+"?", ""),
+"@", ""),
+"[", ""),
+"\\", "-"),
+"]", ""),
+"^", "-"),
+"_", "-"),
+"`", "-"),
+"{", ""),
+"|", "-"),
+"}", ""),
+"~", ""),
+"«", ""),
+"»", ""),
+"€", ""),
+"„", ""),
+"“", ""),
+"-----", "-"),
+"-----", "-"),
+"----", "-"),
+"---", "-"),
+"--", "-")
