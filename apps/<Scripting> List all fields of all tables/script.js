fc0a1e94f26d8e90220ef03c715566576555c75c
Vadorequest <ambroise.dhenain@gmail.com>
2023-03-05 15:20:12 +0100

    Update script.js

--- apps/<Scripting> List all fields of all tables/script.js
+++ apps/<Scripting> List all fields of all tables/script.js
@@ -1,4 +1,5 @@
 for (const table of base.tables) {
-    output.markdown(`# ${table.name} (${table.id})`);
-    output.table(table.fields)
+    output.markdown(`# ${table.name} (${table.id}) [*Lien*](${table.url})`);
+    output.markdown(`###### ${table.description}`);
+    output.table(table.fields);
 }
